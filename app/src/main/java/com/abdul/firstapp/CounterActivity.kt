6f796a3ed6b63ebe2e01cb3fde0e0a28a04c964d
aalshawa97 <abdullahalshawa@gmail.com>
2021-09-07 10:46:26 -0700

    Counter

--- app/src/main/java/com/abdul/firstapp/CounterActivity.kt
+++ app/src/main/java/com/abdul/firstapp/CounterActivity.kt
@@ -2,10 +2,20 @@ package com.abdul.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class CounterActivity : AppCompatActivity() {
+    lateinit var tvCounter: TextView
+    var counter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_counter)
+        tvCounter = findViewById(R.id.tvCounter)
+    }
+
+    fun updateCounter(view: android.view.View) {
+        var currentValue = Integer.parseInt(tvCounter.text.toString())
+
+        tvCounter.setText("" + counter++)
     }
 }
