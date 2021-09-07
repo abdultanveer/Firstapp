a5b6d7d745d3c32515956a09e1e2e82cd4ac211e
abdul <abdul.tanveer@gmail.com>
2021-09-07 23:19:14 +0530

    counter with toast

--- app/src/main/java/com/abdul/firstapp/CounterActivity.kt
+++ app/src/main/java/com/abdul/firstapp/CounterActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 
 class CounterActivity : AppCompatActivity() {
     lateinit var tvCounter: TextView
@@ -18,5 +19,9 @@ class CounterActivity : AppCompatActivity() {
     fun updateCounter(view: View) {
         var currentValue = Integer.parseInt(tvCounter.text.toString())
         tvCounter.setText(""+counter++)
+    }
+
+    fun showToast(view: View) {
+        Toast.makeText(this,""+counter++,Toast.LENGTH_SHORT).show()
     }
 }
