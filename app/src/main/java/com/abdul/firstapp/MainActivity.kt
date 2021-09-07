09258e398533f80e541a2681587318d638c9d9b4
abdul <abdul.tanveer@gmail.com>
2021-09-07 21:48:26 +0530

    button click -- edittext to textview

--- app/src/main/java/com/abdul/firstapp/MainActivity.kt
+++ app/src/main/java/com/abdul/firstapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.abdul.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var etName: EditText   //declartion
+    lateinit var tvRes: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        etName = findViewById(R.id.etName) //initialization
+        tvRes = findViewById(R.id.tvResult)
+    }
+
+    fun clickHandler(view: View) {
+        var name = etName.text.toString()
+        tvRes.setText(name)
     }
 }
