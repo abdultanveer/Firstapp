494f8d4c08e7165af28dc5e0b4e4363b9e8f6bbc
abdul <abdul.tanveer@gmail.com>
2021-09-11 02:24:29 +0530

    setting timer

--- app/src/main/java/com/abdul/firstapp/CounterActivity.kt
+++ app/src/main/java/com/abdul/firstapp/CounterActivity.kt
@@ -3,6 +3,7 @@ package com.abdul.firstapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
 import android.util.Log
 import android.view.View
 import android.widget.EditText
@@ -61,8 +62,10 @@ class CounterActivity : AppCompatActivity() {
 
 
     fun updateCounter(view: View) {
-        var currentValue = Integer.parseInt(tvCounter.text.toString())
-        tvCounter.setText(""+counter++)
+        var seconds = Integer.parseInt(etContact.text.toString())
+        startTimer("sample",seconds)
+        /*var currentValue = Integer.parseInt(tvCounter.text.toString())
+        tvCounter.setText(""+counter++)*/
     }
 
     fun showToast(view: View) {
@@ -72,5 +75,16 @@ class CounterActivity : AppCompatActivity() {
         setResult(RESULT_OK,intent)
         finish()
         //Toast.makeText(this,""+counter++,Toast.LENGTH_SHORT).show()
+    }
+
+    fun startTimer(message: String, seconds: Int) {
+        val intent = Intent(AlarmClock.ACTION_SET_TIMER).apply {
+            putExtra(AlarmClock.EXTRA_MESSAGE, message)
+            putExtra(AlarmClock.EXTRA_LENGTH, seconds)
+            putExtra(AlarmClock.EXTRA_SKIP_UI, true)
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
     }
 }
