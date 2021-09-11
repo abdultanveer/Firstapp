1073ae68d5b32a77ea683eda1fd87596577d7ecf
aalshawa97 <abdullahalshawa@gmail.com>
2021-09-11 11:32:16 -0700

    Time picker

--- app/src/main/java/com/abdul/firstapp/DisplayAlarmActivity.kt
+++ app/src/main/java/com/abdul/firstapp/DisplayAlarmActivity.kt
@@ -1,13 +1,27 @@
 package com.abdul.firstapp
 
+import android.app.Activity
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.app.TimePickerDialog
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.TimePicker
 import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import java.text.SimpleDateFormat
 import java.util.*
+import android.view.LayoutInflater
+
+
+
 
 class DisplayAlarmActivity : AppCompatActivity() {
     /*
@@ -18,17 +32,45 @@ class DisplayAlarmActivity : AppCompatActivity() {
 
     private lateinit var tasksHelper: TaskViewsDisplayHelper
     */
+    lateinit var btnSetAlarm: Button
+    lateinit var timePicker: TimePicker
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_alarm)
-
+        title = "Alarm"
+        timePicker = findViewById(R.id.timePicker)
+        btnSetAlarm = findViewById(R.id.buttonAlarm)
+        btnSetAlarm.setOnClickListener {
+            val calendar: Calendar = Calendar.getInstance()
+            if (Build.VERSION.SDK_INT >= 23) {
+                calendar.set(
+                    calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH),
+                    timePicker.hour,
+                    timePicker.minute,
+                    0
+                )
+            } else {
+                calendar.set(
+                    calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH),
+                    timePicker.currentHour,
+                    timePicker.currentMinute, 0
+                )
+            }
+            setAlarm(calendar.timeInMillis)
+        }
+    }
         //Get reference to button
-        val selectTime = findViewById(R.id.selectTime) as Button
+        //val selectTime = findViewById(R.id.selectTime) as Button
         //Get reference to text view
-        val timeTv = findViewById(R.id.timeTv) as TextView
+        //val timeTv = findViewById(R.id.timeTv) as TextView
 
         //Button click to show TimePicker Dialog
+        /*
         selectTime.setOnClickListener {
             Toast.makeText(this,"Selecting time...", Toast.LENGTH_LONG).show()
             val cal = Calendar.getInstance()
@@ -44,6 +86,30 @@ class DisplayAlarmActivity : AppCompatActivity() {
                 // After button click - open dialog
             }
             TimePickerDialog(this, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
+            setAlarm(cal.timeInMillis)
+        }
+        */
+
+    private fun setAlarm(timeInMillis: Long) {
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(this, MyAlarm::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0)
+        alarmManager.setRepeating(
+            AlarmManager.RTC,
+            timeInMillis,
+            AlarmManager.INTERVAL_DAY,
+            pendingIntent
+        )
+        Toast.makeText(this, "Alarm is set", Toast.LENGTH_SHORT).show()
+    }
+    private class MyAlarm : BroadcastReceiver() {
+        override fun onReceive(
+            context: Context,
+            intent: Intent
+        ) {
+            Log.d("Alarm Bell", "Alarm just fired")
         }
     }
 }
+
+
