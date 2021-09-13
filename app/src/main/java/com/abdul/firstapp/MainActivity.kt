ef0aca96e9af5cc24d5646c5de222c144e41868c
abdul <abdul.tanveer@gmail.com>
2021-09-13 23:48:43 +0530

    arrays spinner onitemselected listener kotlin

--- app/src/main/java/com/abdul/firstapp/MainActivity.kt
+++ app/src/main/java/com/abdul/firstapp/MainActivity.kt
@@ -5,18 +5,20 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     lateinit var etName: EditText   //declartion
     lateinit var tvRes: TextView
+    lateinit var spinner:Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         etName = findViewById(R.id.etName) //initialization
         tvRes = findViewById(R.id.tvResult)
+        spinner = findViewById(R.id.spinner)
+        spinner.onItemSelectedListener = this
     }
 
     fun clickHandler(view: View) {
@@ -41,6 +43,14 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, dataIntent)
         var contact = dataIntent?.getStringExtra("ckey")
         tvRes.setText(contact)
+    }
+
+    override fun onItemSelected(adapterView: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        var itemSelected = adapterView?.selectedItem.toString()
+        Toast.makeText(this,itemSelected,Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
     }
 
 
