a2c65994bd311cb53dfb9e61dfaf88c4a2f324e7
abdul <abdul.tanveer@gmail.com>
2021-09-13 19:40:39 +0530

    button onclicklistener -- callback

---
+++ app/src/main/java/com/abdul/firstapp/HomeActivity.java
@@ -0,0 +1,27 @@
+package com.abdul.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+    Button toastButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        toastButton = findViewById(R.id.btnMessage);
+        //clerk.getSomething(mobilePhone)
+        toastButton.setOnClickListener(this); //button is user who's clicking the switch onclick
+
+    }
+
+    @Override
+    public void onClick(View view) { //switch
+        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show();
+    }
+}
