e3bfa6d9d6ba8827414e1944a9b53b35c9988d5b
abdul <abdul.tanveer@gmail.com>
2021-09-14 23:08:50 +0530

    recyclerview part-1c

--- app/src/main/java/com/abdul/firstapp/RecyclerActivity.kt
+++ app/src/main/java/com/abdul/firstapp/RecyclerActivity.kt
@@ -13,6 +13,8 @@ class RecyclerActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         wordsRecyclerView = findViewById(R.id.wordsRecyclerview)
+        var adapter = WordsAdapter(words)
         wordsRecyclerView.layoutManager = LinearLayoutManager(this)
+        wordsRecyclerView.adapter = adapter
     }
 }
--- app/src/main/java/com/abdul/firstapp/WordsAdapter.kt
+++ app/src/main/java/com/abdul/firstapp/WordsAdapter.kt
@@ -1,25 +1,31 @@
 package com.abdul.firstapp
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.abdul.firstapp.WordsAdapter.WordsViewHolder
 
-class WordsAdapter: RecyclerView.Adapter<WordsViewHolder>() {
+class WordsAdapter(val words: Array<String>) : RecyclerView.Adapter<WordsViewHolder>() {
 
 
-    class WordsViewHolder: RecyclerView.ViewHolder {
-
+    class WordsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvRowPlank : TextView = itemView.findViewById(R.id.tvRow)
     }
 
+    //david -- to buy new planks as and when needed
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordsViewHolder {
-        TODO("Not yet implemented")
+        var plankRow = LayoutInflater.from(parent.context).inflate(R.layout.row_recyclerview,parent,false);
+        return WordsViewHolder(plankRow);
     }
 
+    //abdullah handwriting-- write the content on the plank
     override fun onBindViewHolder(holder: WordsViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.tvRowPlank.setText(words[position])
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return words.size
     }
 }
