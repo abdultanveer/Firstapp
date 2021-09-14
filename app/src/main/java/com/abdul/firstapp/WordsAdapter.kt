5cbadd694ecf1f5b03048df72a2f8f2d6d397a53
abdul <abdul.tanveer@gmail.com>
2021-09-14 22:56:25 +0530

    recyclerview part-1b

--- app/src/main/java/com/abdul/firstapp/WordsAdapter.kt
+++ app/src/main/java/com/abdul/firstapp/WordsAdapter.kt
@@ -1,6 +1,25 @@
 package com.abdul.firstapp
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.abdul.firstapp.WordsAdapter.WordsViewHolder
 
-class WordsAdapter: RecyclerView.Adapter<WordsAdapter.WordsViewHolder>() {
+class WordsAdapter: RecyclerView.Adapter<WordsViewHolder>() {
+
+
+    class WordsViewHolder: RecyclerView.ViewHolder {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordsViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: WordsViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
