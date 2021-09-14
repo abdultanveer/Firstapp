3e26a6a14eb5e1a9c466c0bc724d132d71a0d620
aalshawa97 <abdullahalshawa@gmail.com>
2021-09-14 06:50:24 -0700

    Adding button in Java

--- app/src/main/java/com/abdul/firstapp/HomeActivity.java
+++ app/src/main/java/com/abdul/firstapp/HomeActivity.java
@@ -1,16 +1,21 @@
 package com.abdul.firstapp;
 
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 import static java.sql.DriverManager.println;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MenuInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -33,6 +38,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         } catch (exc: Throwable) {
             println("Something went wrong")
         }*/
+        //Layout inflater
         setContentView(R.layout.activity_home);
         //Socket
         ListView langsListView = findViewById(R.id.languageslv);
@@ -56,7 +62,23 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view){
         Toast.makeText(this, "Button clicked", Toast.LENGTH_LONG).show();
+        //Intent i = new Intent(HomeActivity.this, MainActivity.class);
+        //String strName = null;
+        //i.putExtra("Gender", strName);
 
+        //String newString;
+        /*
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if(extras == null) {
+                newString= null;
+            } else {
+                newString= extras.getString("STRING_I_NEED");
+            }
+        } else {
+            newString= (String) savedInstanceState.getSerializable("STRING_I_NEED");
+        }
+        */
         //Toast.makeText(this, "Gender: " + spinner., Toast.LENGTH_LONG).show();
         /*
         SmsManager manager = SmsManager.getDefault();
@@ -83,6 +105,16 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         //Toast.makeText(this, "Item was selected!",Toast.LENGTH_LONG).show();
         String itemSelected = parent.getSelectedItem().toString();
         Toast.makeText(this, itemSelected, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Button mButton = new Button(this);
+        mButton.setText("send");
+        mButton.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        ConstraintLayout cl = findViewById(R.id.homeConstraintLayout);
+        cl.addView(mButton);
     }
 
     @Override
