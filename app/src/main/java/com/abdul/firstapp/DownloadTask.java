d593b96ee61ad2f70dfaa292c476fcefea4f7cfc
abdul <abdul.tanveer@gmail.com>
2021-09-15 20:48:47 +0530

    async part 3

--- app/src/main/java/com/abdul/firstapp/DownloadTask.java
+++ app/src/main/java/com/abdul/firstapp/DownloadTask.java
@@ -3,6 +3,7 @@ package com.abdul.firstapp;
 
 import android.os.AsyncTask;
 import android.util.Log;
+import android.view.View;
 import android.widget.ProgressBar;
 
 //input type = url= string, progress type = int, result type
@@ -15,9 +16,18 @@ public class DownloadTask extends AsyncTask<String,Integer,Void> {
     }
 
     @Override
+    protected void onPreExecute() { //runs on the ui thread
+        super.onPreExecute();
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
     protected Void doInBackground(String... strings) { //this method will run on a background worker thread
         Log.i(TAG,"downloading--"+strings[0]);
-        publishProgress(75); //sending a message
+        for(int i = 1; i<20; i++) {
+            publishProgress(i*5);//sending a message
+
+        }
         return null;
     }
 
@@ -25,5 +35,11 @@ public class DownloadTask extends AsyncTask<String,Integer,Void> {
     protected void onProgressUpdate(Integer... values) { //receiving message -- this method runs on ui thread
         super.onProgressUpdate(values);
         progressBar.setProgress(values[0]);
+    }
+
+    @Override
+    protected void onPostExecute(Void unused) {
+        super.onPostExecute(unused);
+        //progressBar.setVisibility(View.INVISIBLE);
     }
 }
