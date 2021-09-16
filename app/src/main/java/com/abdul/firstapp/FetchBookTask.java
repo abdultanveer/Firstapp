6758f4cc68be4c6b18ea034f02af76814c8b6475
abdul <abdul.tanveer@gmail.com>
2021-09-16 19:48:27 +0530

    connecting to internet part3 - parsing json

--- app/src/main/java/com/abdul/firstapp/FetchBookTask.java
+++ app/src/main/java/com/abdul/firstapp/FetchBookTask.java
@@ -3,11 +3,14 @@ package com.abdul.firstapp;
 import android.os.AsyncTask;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 //ip type is bookname-- string, op type is a jsonString
 
-public class FetchBookTask extends AsyncTask<String,Void,String> {
+public class FetchBookTask extends AsyncTask<String, Void, String> {
     TextView titleTv, authorTv;
 
     public FetchBookTask(TextView mTitleText, TextView mAuthorText) {
@@ -23,5 +26,39 @@ public class FetchBookTask extends AsyncTask<String,Void,String> {
     @Override
     protected void onPostExecute(String jsonString) {
         super.onPostExecute(jsonString);
+
+        try {
+            JSONObject rootJsonObject = new JSONObject(jsonString);
+            JSONArray itemsArray = rootJsonObject.getJSONArray("items");
+            for (int i = 0; i < itemsArray.length(); i++) {
+                JSONObject book = itemsArray.getJSONObject(i);
+                String title = null;
+                String authors = null;
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                try {
+                    title = volumeInfo.getString("title");
+                    authors = volumeInfo.getString("authors");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                if (title != null && authors != null) {
+                    titleTv.setText(title);
+                    authorTv.setText(authors);
+                    return;
+                }
+
+                titleTv.setText("No Results Found");
+                authorTv.setText("");
+
+            }
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
