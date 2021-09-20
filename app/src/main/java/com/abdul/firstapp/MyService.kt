202dabb4a300496759f1be6181d690140539db7d
abdul <abdul.tanveer@gmail.com>
2021-09-21 00:37:16 +0530

    bind to a service

--- app/src/main/java/com/abdul/firstapp/MyService.kt
+++ app/src/main/java/com/abdul/firstapp/MyService.kt
@@ -3,34 +3,42 @@ package com.abdul.firstapp
 import android.app.Service
 import android.content.Intent
 import android.media.MediaPlayer
+import android.os.Binder
 import android.os.IBinder
 import android.util.Log
-
+//server -- addition 2 nos or location data, ads
 class MyService : Service() {
     var TAG = MyService::class.java.simpleName
+
+    private val addBinder = LocalBinder()
+
+    //binder is like pipe through which data flows
+    inner class LocalBinder : Binder() {
+        // Return this instance of LocalService so clients can call public methods
+        public fun getService(): MyService {
+            return MyService()
+        }
+    }
+
+
+
     override fun onCreate() {
         super.onCreate()
         Log.i(TAG,"service created")
     }
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-         super.onStartCommand(intent, flags, startId)
-        var name = intent?.getStringExtra("musicfile")
-        Log.i(TAG,"i am going to play--"+name)
-        var mediaPlayer: MediaPlayer = MediaPlayer.create(this,R.raw.music)
-        mediaPlayer.start()
-        return START_STICKY
-
-    }
 
     override fun onBind(intent: Intent): IBinder? {
-        return null
+        return addBinder
     }
 
 
     override fun onDestroy() {
         super.onDestroy()
         Log.i(TAG,"service destroyed")
+    }
 
+    fun add(a:Int, b:Int):Int {
+        return a+b
     }
 }
--- app/src/main/java/com/abdul/firstapp/ServiceDemoActivity.kt
+++ app/src/main/java/com/abdul/firstapp/ServiceDemoActivity.kt
@@ -1,10 +1,15 @@
 package com.abdul.firstapp
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.view.View
+import android.widget.Toast
 
+//client
 class ServiceDemoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,13 +20,31 @@ class ServiceDemoActivity : AppCompatActivity() {
         when(view.id){
             R.id.btnStartServ -> {
                 var serviceIntent:Intent = Intent(this,MyService::class.java)
-                serviceIntent.putExtra("musicfile","mymusic.mp3")
-                startService(serviceIntent)
+                //serviceIntent.putExtra("musicfile","mymusic.mp3")
+
+                //startService(serviceIntent)  //create a service object
+
+                bindService(serviceIntent,serviceConnection, BIND_AUTO_CREATE)
             }
             R.id.btnStop -> {
                 var serviceIntent1:Intent = Intent(this,MyService::class.java)
                 stopService(serviceIntent1)
             }
+        }
+    }
+
+    private lateinit var mService: MyService
+
+    //serviceConnection like a contract
+    private val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(p0: ComponentName?, addBinder: IBinder?) {
+            val binder = addBinder as MyService.LocalBinder
+            mService = binder.getService()  //i am not creating a new object instead i am binding to an existing object and consuming its services
+            var result = mService.add(10,25)
+            Toast.makeText(applicationContext,"result = "+result,Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
         }
     }
 }
